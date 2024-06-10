554af4c99b9c355aeaecffe0974e37cbcfce64ee
Luan Costa PC <luan-esposito@hotmail.com>
2024-06-09 21:33:04 -0300

    features funcionais do modal carrinho adicionadas(aumentar, diminuir e remover itens do carrinho)

--- js/app.js
+++ js/app.js
@@ -145,12 +145,143 @@ cardapio.metodos = {
 
         if (abrir) {
             $("#modalCarrinho").removeClass('hidden');
+            cardapio.metodos.carregarCarrinho();
         }
         else {
             $("#modalCarrinho").addClass('hidden');
         }
-        
+
     },
+
+    //altera os textos e exibe os botões das etapas
+    carregarEtapa: (etapa) => {
+        if (etapa == 1) {
+            $("#lblTituloEtapa").text('Seu carrinho: ');
+            $("#itensCarrinho").removeClass('hidden');
+            $("#localEntrega").addClass('hidden');
+            $("#resumoCarrinho").addClass('hidden');
+
+            $(".etapa").removeClass('active');
+            $(".etapa1").addClass('active');
+
+            $("#btnEtapaPedido").removeClass('hidden');
+            $("#btnEtapaEndereco").addClass('hidden');
+            $("#btnEtapaResumo").addClass('hidden');
+            $("#btnVoltar").addClass('hidden');
+        }
+        if (etapa == 2) {
+            $("#lblTituloEtapa").text('Endereço de entrega: ');
+            $("#itensCarrinho").addClass('hidden');
+            $("#localEntrega").removeClass('hidden');
+            $("#resumoCarrinho").addClass('hidden');
+
+            $(".etapa").removeClass('active');
+            $(".etapa1").addClass('active');
+            $(".etapa2").addClass('active');
+
+            $("#btnEtapaPedido").addClass('hidden');
+            $("#btnEtapaEndereco").removeClass('hidden');
+            $("#btnEtapaResumo").addClass('hidden');
+            $("#btnVoltar").removeClass('hidden');
+        }
+        if (etapa == 3) {
+            $("#lblTituloEtapa").text('Resumo do pedido: ');
+            $("#itensCarrinho").addClass('hidden');
+            $("#localEntrega").addClass('hidden');
+            $("#resumoCarrinho").removeClass('hidden');
+
+            $(".etapa").removeClass('active');
+            $(".etapa1").addClass('active');
+            $(".etapa2").addClass('active');
+            $(".etapa3").addClass('active');
+
+            $("#btnEtapaPedido").addClass('hidden');
+            $("#btnEtapaEndereco").addClass('hidden');
+            $("#btnEtapaResumo").removeClass('hidden');
+            $("#btnVoltar").removeClass('hidden');
+        }
+    },
+
+    //botão de voltar etapa
+    voltarEtapa: () => {
+
+        let etapa = $(".etapa.active").length;
+        cardapio.metodos.carregarEtapa(etapa - 1);
+
+    },
+
+    // carrega a lista de itens do carrinho
+    carregarCarrinho: () => {
+
+        cardapio.metodos.carregarEtapa(1);
+
+        if (MEU_CARRINHO.length > 0) {
+
+            $("#itensCarrinho").html('');
+
+            $.each(MEU_CARRINHO, (i, e) => {
+
+                let template = cardapio.templates.itemCarrinho.replace(/\${img}/g, e.img)
+                    .replace(/\${nome}/g, e.name)
+                    .replace(/\${preco}/g, e.price.toFixed(2).replace('.', ','))
+                    .replace(/\${id}/g, e.id)
+                    .replace(/\${qtd}/g, e.qtd);
+
+                $("#itensCarrinho").append(template);
+            })
+
+        } else {
+            $("#itensCarrinho").html('<p class="carrinho-vazio"><i class="fa fa-shopping-bag"></i> Seu carrinho está vazio.</p>');
+
+        }
+
+    },
+
+    //diminuir quantidade do item no carrinho
+    diminuirQuantidadeCarrinho: (id) => {
+
+        let qtdAtual = parseInt($("#qtd-carrinho-" + id).text());
+
+        if (qtdAtual > 1) {
+            $("#qtd-carrinho-" + id).text(qtdAtual - 1);
+            cardapio.metodos.atualizarCarrinho(id, qtdAtual - 1);
+        } else {
+            cardapio.metodos.removerItemCarrinho(id)
+        }
+
+    },
+
+    //aumentar quantidade do item no carrinho
+    aumentarQuantidadeCarrinho: (id) => {
+
+        let qtdAtual = parseInt($("#qtd-carrinho-" + id).text());
+        $("#qtd-carrinho-" + id).text(qtdAtual + 1);
+        cardapio.metodos.atualizarCarrinho(id, qtdAtual + 1);
+
+    },
+
+    // bitão remover item do carrinho
+    removerItemCarrinho: (id) => {
+
+        MEU_CARRINHO = $.grep(MEU_CARRINHO, (e, i) => { return e.id != id });
+        cardapio.metodos.carregarCarrinho();
+
+        cardapio.metodos.atualizarbadgeTotal();
+
+    },
+
+    //atualiza o carrinho com a quantidade atual
+    atualizarCarrinho: (id, qtd) => {
+
+        let objIndex = MEU_CARRINHO.findIndex((obj => obj.id == id));
+        MEU_CARRINHO[objIndex].qtd = qtd;
+
+        //atualiza o botão carrinho com a quantidade atualizada
+        cardapio.metodos.atualizarbadgeTotal();
+
+    },
+
+
 
     //Mensagens
     mensagem: (texto, cor = 'red', tempo = 3500) => {
@@ -197,6 +328,30 @@ cardapio.templates = {
                             </div>
                         </div>
 
+    `,
+
+    itemCarrinho: `
+    
+                    <div class="col-12 item-carrinho">
+                        <div class="img-produto">
+                            <img
+                                src="\${img}">
+                        </div>
+                        <div class="dados-produto">
+                            <p class="title-produto"><b>\${nome}</b></p>
+                            <p class="price-produto"><b>R$ \${preco}</b></p>
+                        </div>
+                        <div class="add-carrinho">
+                           <span class="btn-menos" onclick="cardapio.metodos.diminuirQuantidadeCarrinho('\${id}')"><i class="fa fa-minus"></i></span>
+                                    <span class="add-numero-itens" id="qtd-carrinho-\${id}">\${qtd}</span>
+                                    <span class="btn-mais" onclick="cardapio.metodos.aumentarQuantidadeCarrinho('\${id}')"><i class="fa fa-plus"></i></span>
+                            <span class="btn btn-remove" onclick="cardapio.metodos.removerItemCarrinho('\${id}')"><i class="fa fa-times"></i></span>
+                        </div>
+                    </div>
+
+
     `
+
+
 
 }
